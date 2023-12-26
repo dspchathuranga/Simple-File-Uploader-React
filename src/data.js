63413807824ff08e72bf744c5fa542d01b44b048
DSP Chathuranga <dspchathuranga@gmail.com>
2023-12-26 11:51:54 +0000

    initial commit

---
+++ src/data.js
@@ -0,0 +1,50 @@
+export const accountIDs = [
+  {
+    id: '1234-5678-9012',
+    name: "Account 1",
+  },
+
+  {
+    id: '3456-7890-1234',
+    name: "Account 2",
+  },
+  {
+    id: '5678-9012-3456',
+    name: "Account 3",
+  },
+  {
+    id: '7890-1234-5678',
+    name: "Account 4",
+  },
+  {
+    id: '9012-3456-7890',
+    name: "Account 5",
+    
+  },
+  
+];
+
+export const bucketNames = [
+  {
+    id: 1,
+    name: "my-sample-bucket-123",
+  },
+
+  {
+    id: 2,
+    name: "aws-example-bucket-456",
+  },
+  {
+    id: 3,
+    name: "sample-s3-bucket-789",
+  },
+  {
+    id: 4,
+    name: "project-bucket-abc",
+  },
+  {
+    id: 5,
+    name: "dev-test-s3-bucket-xyz",
+    
+  },
+];
